d514ebc46457777e83856c048a729550a9a87996
CristineTio <tiocristinejoy@gmail.com>
2025-02-08 23:03:10 +0800

    Delete TA2-1_TIO.py

--- TA2-1_TIO.py
+++
@@ -1,7 +0,0 @@
-firstName = input("Enter your first name: ")
-lastName = input("Enter your last name: ")
-age = int(input("Enter your age: "))
-
-print("Hello, " + firstName + " " + lastName + "!")
-print(firstName[0:4])
-print("Greeting Message: " + "Hello, " + firstName[0:4] + "!" + " " +"Welcome. You are", age, "years old.")
